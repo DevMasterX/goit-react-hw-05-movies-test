13f7a7be881669ed8977423e9be962805c1df1ce
DevMasterX <sokol080288@gmail.com>
2023-10-29 17:03:14 +0200

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+// import Lauout from "./Layout/Lauout";
+import Home from "pages/Home/Home";
+import Movies from "pages/Movies/Movies";
+import MovieDetails from "pages/MovieDetails/MovieDetails";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/movies" element={<Movies/>}/>
+      <Route path="/movies/:movieId" element={<MovieDetails/>}/>
+      <Route path="*" element={<Navigate to="/"/>}/>
+    </Routes>
   );
 };
---
+++ src/components/Layout/Lauout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lauout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Lauout
